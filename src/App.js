c43d4c889c9190e18a081afa592c56298ddb50b7
soph-th <109481663+soph-th@users.noreply.github.com>
2022-12-09 11:18:24 +0000

    present component created and css started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Present from './Present';
 
 function App() {
   return (
     <div className="App">
       <h1>CAS in da house</h1>
+      <Present />
     </div>
   );
 }
--- src/Present.js
+++ src/Present.js
@@ -1,2 +1,17 @@
-//divs that make up present
+import './present.css';
 
+
+export default function Present() {
+    return(
+        <div className="present">
+            <div className="lid"></div>
+                <span></span>
+            <div className="activity">
+                <a href='http://www.google.com'>Link Text</a>
+            </div>
+            <div className="box"></div>
+            <span></span>
+            <span></span>
+        </div>
+    )
+}
