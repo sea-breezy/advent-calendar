ea46e10d06c99723d439db4fb73713ee533d9b92
camillabrizzi <camillabrizzi@icloud.com>
2022-12-09 15:16:52 +0000

    embedded audio player

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Present from "./components/Present";
 import Calendar from "./components/Calendar";
-
+import "./music.mp3";
+import ReactPlayer from "react-player";
 
 function App() {
   return (
     <div className="App">
+      <ReactPlayer
+        url="https://soundcloud.com/christmasmusicofficial/we-wish-you-a-merry-8?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
+        width="10%"
+        height="10%"
+      />
       <h1>CAS in da house</h1>
       <Calendar />
     </div>
