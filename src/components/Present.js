a8e562d5ebff91e3e04adf65edb151b1219014a1
camillabrizzi <camillabrizzi@icloud.com>
2022-12-09 11:31:14 +0000

    moved spans again

--- src/components/Present.js
+++ src/components/Present.js
@@ -3,8 +3,9 @@ import "./present.css";
 export default function Present({ link, text }) {
   return (
     <div className="present">
-      <div className="lid"></div>
-      <span></span>
+      <div className="lid">
+        <span></span>
+      </div>
       <div className="activity">
         <a href={link}>{text}</a>
       </div>
