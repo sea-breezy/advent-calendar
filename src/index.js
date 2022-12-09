659cc8a220dfa734d7abff1ee3ee1c3a6bfe58b1
aliafarhana88 <alia.abdulghaffar@gmail.com>
2022-12-09 15:21:56 +0000

    snowflakes background added

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Snowfall from 'react-snowfall'; //React Snowfall package
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Snowfall />
   </React.StrictMode>
 );
 
