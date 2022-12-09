35b55d75882ea182f87f6de4b567db072b21c550
camillabrizzi <camillabrizzi@icloud.com>
2022-12-09 16:18:01 +0000

    resized music player

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import Calendar from "./components/Calendar";
-import "./music.mp3";
 import ReactPlayer from "react-player";
 import Snowfall from "react-snowfall"; //React Snowfall package
 import { useState, useEffect } from "react";
@@ -18,8 +17,8 @@ function App() {
     <div className="App">
       <ReactPlayer
         url="https://soundcloud.com/christmasmusicofficial/we-wish-you-a-merry-8?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
-        width="10%"
-        height="10%"
+        width="5%"
+        height="5%"
       />
       <header>
         <h1 className="shimmer">Advent Calendar of Festive Activities</h1>
